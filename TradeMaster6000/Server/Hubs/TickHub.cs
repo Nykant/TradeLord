30a1f2dbd467c4674043e5161238b7f028603f7e
Christian <Christian@DESKTOP-GB0B2FU>
2021-05-11 01:40:24 +0200

    sheesh

--- TradeMaster6000/Server/Hubs/TickHub.cs
+++ TradeMaster6000/Server/Hubs/TickHub.cs
@@ -15,25 +15,21 @@ using TradeMaster6000.Shared;
 
 namespace TradeMaster6000.Server.Hubs
 {
+
     public class TickHub : Hub
     {
         private static List<TradeOrder> orderList = new List<TradeOrder>();
         private static List<string> logList = new List<string>();
-        //private readonly IWorker worker;
-        //private readonly IWorker2 worker2;
-        private readonly ILogger<Worker> logger;
+        public static List<Order> orderDataList;
+        private readonly ILogger<OrderWork> logger;
         private readonly IKiteService kiteService;
         private readonly IHttpContextAccessor _contextAccessor;
         private readonly IConfiguration _configuration;
         private readonly IInstrumentService instrumentService;
-        // måske gem tickers her inde i stedet for i worker
         private static int OrderCount { get; set; }
-        // kan bruge samme worker 
 
-        public TickHub(/*IWorker worker, IWorker2 worker2, */ILogger<Worker> logger, IConfiguration configuration, IHttpContextAccessor contextAccessor, IKiteService kiteService, IInstrumentService instrumentService)
+        public TickHub(ILogger<OrderWork> logger, IConfiguration configuration, IHttpContextAccessor contextAccessor, IKiteService kiteService, IInstrumentService instrumentService)
         {
-            //this.worker = worker;
-            //this.worker2 = worker2;
             this.logger = logger;
             _configuration = configuration;
             _contextAccessor = contextAccessor;
@@ -41,11 +37,15 @@ namespace TradeMaster6000.Server.Hubs
             this.instrumentService = instrumentService;
         }
 
-        public async Task StartTicker(TradeOrder order)
+        // start order work with inputs from user 
+        public async Task StartOrderWork(TradeOrder order)
         {
-            Worker worker = new Worker(logger, _configuration, _contextAccessor, kiteService);
+            // new instance of OrderWork class
+            OrderWork orderWork = new OrderWork(logger, _configuration, _contextAccessor, kiteService);
             order.TokenSource = new CancellationTokenSource();
             order.Id = OrderCount;
+
+            // set order instrument with tradingsymbol input from user
             foreach(var instrument in instrumentService.GetInstruments())
             {
                 if(instrument.TradingSymbol == order.TradeSymbol.ToString())
@@ -54,14 +54,21 @@ namespace TradeMaster6000.Server.Hubs
                 }
             }
 
+            // add the OrderWork instance to a list (which stays for application lifetime)
             orderList.Add(order);
             OrderCount = OrderCount + 1;
 
             await Clients.Caller.SendAsync("ReceiveList", orderList);
 
-            await Task.Run(async () => 
+            // get available threads and log amount
+            ThreadPool.GetAvailableThreads(out int workerThreads, out int completionPortThreads);
+            AddLog($"log: maximum number of threads available: {workerThreads}");
+
+            // start work in that instance which runs 
+            await Task.Run( () => 
             {
-                await worker.StartTicker(Clients, this, order, order.TokenSource.Token);
+                orderWork.StartWork(Clients, this, order, order.TokenSource.Token);
+                AddLog($"log: Task {Task.CurrentId} has finished");
             });
         }
         public async Task GetInstruments()
@@ -73,18 +80,21 @@ namespace TradeMaster6000.Server.Hubs
         {
             await Clients.Caller.SendAsync("ReceiveList", orderList);
         }
+        public async Task GetOrderData()
+        {
+            await Clients.Caller.SendAsync("ReceiveOrderData", orderDataList);
+        }
         public async Task GetLogList()
         {
             await Clients.Caller.SendAsync("ReceiveLogs", logList);
         }
-        public async Task AddLog(string log)
+        public void AddLog(string log)
         {
             logList.Add(log);
-            //await Clients.Caller.SendAsync("ReceiveLogs", logList);
         }
 
 
-        public async Task StopTicker(int id)
+        public async Task StopOrderWork(int id)
         {
             if(orderList.Count > 0)
             {
@@ -99,115 +109,5 @@ namespace TradeMaster6000.Server.Hubs
                 }
             }
         }
-
-        public async Task SendTick(decimal tick, IHubCallerClients clients)
-        {
-            await clients.All.SendAsync("ReceiveTick", tick);
-        }
-
-        //public async Task StartTicker2()
-        //{
-        //    //source2 = new CancellationTokenSource();
-
-        //    await worker2.StartTicker(Clients, this);
-        //}
-
-        //public async Task StopTicker2()
-        //{
-        //    //source2.Cancel();
-
-        //    await worker2.StopTicker();
-        //}
-
-        //public async Task SendTick2(decimal tick, IHubCallerClients clients)
-        //{
-        //    await clients.All.SendAsync("ReceiveTick2", tick);
-        //}
     }
-
-    //public interface ITickHub
-    //{
-    //    public Task StartTicker();
-    //    public Task StopTicker();
-    //    public Task SendTick(decimal tick, IHubCallerClients clients);
-    //    public Task StartTicker2();
-    //    public Task StopTicker2();
-    //    public Task SendTick2(decimal tick, IHubCallerClients clients);
-    //}
 }
-
-
-// public async Task GetTicks(CancellationToken cancellationToken)
-//{
-//    ticker.OnTick += onTick;
-//    //ticker.OnOrderUpdate += OnOrderUpdate;
-//    //ticker.OnReconnect += onReconnect;
-//    //ticker.OnNoReconnect += oNoReconnect;
-//    //ticker.OnError += onError;
-//    //ticker.OnClose += onClose;
-//    //ticker.OnConnect += onConnect;
-
-//    ticker.EnableReconnect(Interval: 5, Retries: 50);
-//    ticker.Connect();
-
-//    ticker.Subscribe(Tokens: new UInt32[] { 60417 });
-//    ticker.SetMode(Tokens: new UInt32[] { 60417 }, Mode: Constants.MODE_LTP);
-
-//    while (!cancellationToken.IsCancellationRequested)
-//    {
-
-//    }
-
-//    ticker.Close();
-//}
-//private static void onTick(Tick TickData)
-//{
-//    //await tickHub.SendTick(TickData.LastPrice);
-//}
-
-//public async Task DoWork(CancellationToken cancellationToken)
-//{
-//    // Create a new Ticker instance
-//    Ticker ticker = new Ticker("fpjghegvpdmrifse", Configuration.GetValue<string>("AccessTokenPassword"));
-
-//    ticker.OnTick += onTick;
-//    //ticker.OnOrderUpdate += OnOrderUpdate;
-//    //ticker.OnReconnect += onReconnect;
-//    //ticker.OnNoReconnect += oNoReconnect;
-//    //ticker.OnError += onError;
-//    //ticker.OnClose += onClose;
-//    //ticker.OnConnect += onConnect;
-
-//    ticker.EnableReconnect(Interval: 5, Retries: 50);
-//    ticker.Connect();
-
-//    ticker.Subscribe(Tokens: new UInt32[] { 60417 });
-//    ticker.SetMode(Tokens: new UInt32[] { 60417 }, Mode: Constants.MODE_LTP);
-//    logger.LogInformation($"Subscribed to {60417}");
-
-//    while (!cancellationToken.IsCancellationRequested)
-//    {
-
-//    }
-
-//    ticker.Close();
-//}
-//public async Task SendTick()
-//{
-//    // Create a new Ticker instance
-//    Ticker ticker = new Ticker("fpjghegvpdmrifse", "accesstokenpassword");
-
-//    ticker.OnTick += onTick;
-//    //ticker.OnOrderUpdate += OnOrderUpdate;
-//    //ticker.OnReconnect += onReconnect;
-//    //ticker.OnNoReconnect += oNoReconnect;
-//    //ticker.OnError += onError;
-//    //ticker.OnClose += onClose;
-//    //ticker.OnConnect += onConnect;
-
-//    ticker.EnableReconnect(Interval: 5, Retries: 50);
-//    ticker.Connect();
-
-//    ticker.Subscribe(Tokens: new UInt32[] { 60417 });
-//    ticker.SetMode(Tokens: new UInt32[] { 60417 }, Mode: Constants.MODE_LTP);
-//}
